6d33fb5020d86c88d41793c501a5166accd6736e
Fernando Moya <fernandoalexander2014@gmail.com>
2020-06-06 20:46:57 -0500

    Add Location

--- app/src/main/java/com/fernandomoya/appproyectofinal/HomeActivity.java
+++ app/src/main/java/com/fernandomoya/appproyectofinal/HomeActivity.java
@@ -3,21 +3,51 @@ package com.fernandomoya.appproyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FusedLocationProviderClient fusedLocationClient;
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mDatabase= FirebaseDatabase.getInstance().getReference();
         btnLogout = findViewById(R.id.logout);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Log.e("Latitud: ",+location.getLatitude()+" Longitud: "+ location.getLongitude());
+                            Map<String, Object> latlong= new HashMap<>();
+                            latlong.put("Latitud", location.getLatitude());
+                            latlong.put("Longitud", location.getLongitude());
+                            mDatabase.child("perros").push().setValue(latlong);
+
+                        }
+                    }
+                });
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
