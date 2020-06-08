7f1fd4df75755359a6420aeb19253580b13eb134
Fernando Moya <fernandoalexander2014@gmail.com>
2020-06-07 20:21:52 -0500

    Login Style, Google Maps API

--- app/src/main/java/com/fernandomoya/appproyectofinal/HomeActivity.java
+++ app/src/main/java/com/fernandomoya/appproyectofinal/HomeActivity.java
@@ -1,53 +1,52 @@
 package com.fernandomoya.appproyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.fernandomoya.appproyectofinal.model.Perros;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class HomeActivity extends AppCompatActivity {
-    Button btnLogout;
-    FirebaseAuth mFirebaseAuth;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private  int MY_PERMISSIONS_REQUEST_READ_CONTACTS ;
+    Button btnLogout, btnMaps;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference mDatabase;
+    EditText descripcionP;
+    Perros perros;
+
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private FusedLocationProviderClient fusedLocationClient;
-    DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mDatabase= FirebaseDatabase.getInstance().getReference();
         btnLogout = findViewById(R.id.logout);
-
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            Log.e("Latitud: ",+location.getLatitude()+" Longitud: "+ location.getLongitude());
-                            Map<String, Object> latlong= new HashMap<>();
-                            latlong.put("Latitud", location.getLatitude());
-                            latlong.put("Longitud", location.getLongitude());
-                            mDatabase.child("perros").push().setValue(latlong);
-
-                        }
-                    }
-                });
+        descripcionP= findViewById(R.id.editText3);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,5 +56,59 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intToMain);
             }
         });
+
+        btnMaps=findViewById(R.id.button);
+        btnMaps.setOnClickListener(this);
+        inicializarFirebase();
+    }
+
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabase = firebaseDatabase.getReference();
+    }
+    private void limpiarCajas() {
+        descripcionP.setText("");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+
+
+        switch (view.getId()){
+
+            //case R.id.button: Intent intent= new Intent(HomeActivity.this,MapsActivity.class);
+           case R.id.button:
+               if(ActivityCompat.checkSelfPermission(this,
+                       Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                       && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+                   ActivityCompat.requestPermissions(HomeActivity.this,
+                           new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                           MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+               }
+               fusedLocationClient.getLastLocation()
+                       .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                           @Override
+                           public void onSuccess(Location location) {
+                               String descripcionGeneral =descripcionP.getText().toString();
+                               if (location != null) {
+                                   Perros p= new Perros();
+                                   p.setDescripcion(descripcionGeneral);
+                                   p.setLatitud(String.valueOf(location.getLatitude()));
+                                   p.setLongitud(String.valueOf(location.getLongitude()));
+                                   Log.e("Latitud: ",+location.getLatitude()+" Longitud: "+ location.getLongitude()+" Descripcion: "+p.getDescripcion());
+                                   mDatabase.child("perros").push().setValue(p);
+                                   Toast.makeText(HomeActivity.this,"Agregado",Toast.LENGTH_SHORT).show();
+                               }
+                           }
+                       });
+               limpiarCajas();
+            //startActivity(intent);
+
+            break;
+        }
     }
 }
---
+++ app/src/main/java/com/fernandomoya/appproyectofinal/model/Perros.java
@@ -0,0 +1,59 @@
+package com.fernandomoya.appproyectofinal.model;
+
+public class Perros {
+
+    private String id;
+    private String correo;
+    private String password;
+    private String descripcion;
+    private String latitud;
+    private String longitud;
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+}
